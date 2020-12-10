c332c161bc3e405c408e7a8c8a5649c61beb352b
sammydeni <samueledenicola01@gmail.com>
2020-12-10 12:01:36 +0100

    up

---
+++ c_code/es_lezione_4/max_min_for.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//calcola max e min
+
+int main(int argc, char const *argv[])
+{
+    //dichiaro le variabili
+    int max, min, c, a;
+    printf("Inserisci il primo numero: ");
+    scanf("%d", &max);
+    min = max;
+    //uso un ciclo for
+    for ( c = 0; c < 4; c++)
+        {
+        printf("inserisci un valore: ");
+        scanf("%d", &a);
+        if (a > max)
+            max = a;
+         
+        if (a < min)
+            min = a;
+        }
+    
+    //stampo i risultati
+    printf(" il massimo e' %d\n", max);
+    printf(" il minimo e' %d\n", min);
+    return 0;
+}
