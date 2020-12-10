03d40ba740aa1753bc0a08444fbec1b93aa31a93
sammydeni <samueledenicola01@gmail.com>
2020-12-10 11:33:46 +0100

    up

---
+++ c_code/es_lezione_4/max_min.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int max, min, c, a;
+    printf("Inserisci il primo numero: ");
+    scanf("%d",&a);
+    max = a;
+    min = a;
+    c = 0;
+
+    while (c<4)
+        {
+        printf("inserisci un valore: ");
+        scanf("%d",&a);
+        if (a > max)
+            max = a;
+        else
+            {
+            if (a < min)
+                min = a;
+            }
+        c++;
+        }
+    
+    //stampo i risultati
+    printf(" il massimo e' %d\n", max);
+    printf(" il minimo e' %d\n", min);
+    return 0;
+}
