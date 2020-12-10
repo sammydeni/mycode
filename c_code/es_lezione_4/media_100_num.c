2eb6e793037938bfb660998930b7dab3b652c89c
sammydeni <ri7g6fh6>
2020-12-10 18:06:11 +0100

    up

---
+++ c_code/es_lezione_4/media_100_num.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int i, s=0;
+    float m=0;
+    for(i=0; i<100; i++)
+        {
+        s+=i;
+        }
+    
+    m=(float) s/ 100;
+    printf("La media è %.2f", m);
+    return 0;
+}
