b429874023f463ff860b31f2823af90da747d145
sammydeni <samueledenicola01@gmail.com>
2021-01-07 09:54:08 +0100

    up

---
+++ c_code/es_lezione_5/tabellina3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int t = 3, k = 0;
+    while (k <= 10)
+        {
+        printf("%d\n",t*k);
+        k++;
+        }
+    return 0;
+}
