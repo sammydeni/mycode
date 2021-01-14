72f52f1acff9b6b991e84700ceebb4876e14dc50
Samuele Denicola <samueledenicola01@gmail.com>
2021-01-14 11:54:11 +0100

    fix

--- c_code/16-l7-f_decimale.c
+++ c_code/16-l7-f_decimale.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int leggicifra()
+int leggiCifra()
     {
         int a;
     scanf("%d",&a);
@@ -18,7 +18,7 @@ int main(int argc, char const *argv[])
     int cifra;
 
     printf("Inserisci una cifra tra 0 e 9: ");
-    cifra=leggicifra();
+    cifra=leggiCifra();
     printf("La cifra e' %d\n", cifra);
     return 0;
 }
