600dfe63970131d56825b0a6a2ea3a0a4fb78474
sammydeni <samueledenicola01@gmail.com>
2021-01-25 22:19:06 +0100

    up

---
+++ c_code/22-l9-trasforma_maiuscola.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+char inputLettera()
+    {
+    char a;
+  
+    printf("inserisci una lettera: ");
+    a=getch();
+    while ((a<'a' || a>'z') && (a<'A' || a>'Z'))
+    {
+        a=getch();
+    }
+    return a;
+    }
+
+char inMaiuscola()
+    {
+    char k;
+    k = inputLettera();
+    if (k > 'Z')
+        {
+        k-=32;
+        }
+    return k;
+    }
+
+int main(int argc, char *argv[])
+{
+ char a;
+a = inMaiuscola();
+printf("\nla lettera e': %c\n", a); 
+  return 0;
+}
