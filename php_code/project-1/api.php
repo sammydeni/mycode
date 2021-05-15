9374fe90713e4d2ceac2a23917a50795445bd07d
Samuele Denicola <samueledenicola01@gmail.com>
2021-05-15 19:00:25 +0200

    up

--- php_code/project-1/api.php
+++ php_code/project-1/api.php
@@ -1,3 +1,23 @@
 <?php
 session_start();
+
+
+$valutazioni =  (isset($_SESSION['titles']))?$_SESSION['titles']:[];
+$titoli = (isset($_SESSION['ratings']))?$_SESSION['ratings']:[];
+
+if (    isset($_POST['title']) && isset($_POST['rate'])     )  {
+    $titoli[] = $_POST['title'];
+    $valutazioni[] = $_POST['rate'];
+}
+   
+
+    function stampa($titoli, $valutazioni) {
+        for($i=0; $i < sizeof($titoli); $i++)
+            {
+                echo $titoli[$i]."<br>";
+                echo $valutazioni[$i]."<br>";
+            }
+    }
+        
+
 ?>
--- php_code/project-1/print.php
+++ php_code/project-1/print.php
@@ -1,2 +1,18 @@
 <?php
+    session_start();
+    include 'api.php';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stampa</title>
+</head>
+<body>
+    <div id="box">
+        <?php stampa($titoli, $valutazioni);  ?>
+    </div>
+</body>
+</html>
