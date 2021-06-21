4208607866df62df00be1b467b22f72c35300701
Samuele Denicola <samueledenicola01@gmail.com>
2021-06-21 19:11:23 +0200

    up

--- js_code/13-js-in-depth/03-animals/script/main.js
+++ js_code/13-js-in-depth/03-animals/script/main.js
@@ -1,27 +1,41 @@
 let animals = [
     {
-        username = "Billy",
-        tagline = "Silly Billy",
-        noises =["Huha", "Waa", "HuWaa"],
-        foods =["bananas", "Crackers", "Beers"]
+        username: "",
+        tagline: "",
+        noises: [""],
+        foods: [""]
     },
     {
-        username = "",
-        tagline = "",
-        noises =[],
-        foods =[]
+        username: "",
+        tagline: "",
+        noises: [""],
+        foods: [""]
     },
     {
-        username = "",
-        tagline = "",
-        noises =[],
-        foods =[]
+        username: "",
+        tagline: "",
+        noises: [""],
+        foods: [""]
     },
     {
-        username = "",
-        tagline = "",
-        noises =[],
-        foods =[]
+        username: "",
+        tagline: "",
+        noises: [""],
+        foods: [""]
     }
-
 ];
+
+var i = 0;
+animals.forEach(animal => {
+    const lastName = ["Bob", "Bell", "John", "Sylv"];
+    animal.username = "Billy" + lastName[i];
+    animal.tagline = "Silly Billy " + i;
+    animal.noises = ["Huha", "Waa", "HuWaa"];
+    animal.foods = ["bananas", "Crackers", "Beers"];
+    i++;
+
+    for (const key in animal) {
+        console.log(`${key}: ${animal[key]}`);
+    }
+});
+
